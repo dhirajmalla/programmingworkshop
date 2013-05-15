8c7ca30733dffea3627f5b21820918c178d316a9
dhirajmalla <replytodhiraj@gmail.com>
2013-05-15 12:21:57 +0300

    Update and rename North.java to Mord.java

--- src/spacewar/Mord.java
+++ src/spacewar/Mord.java
@@ -4,19 +4,26 @@ package spacewar;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
-public class North {
+/**
+ * This is class for one of the city
+ * @author Team  Dhiraj  and Ioana
+ * @version 1.0
+ */
+public class Mord implements Level {
     private int x;
     private int y;
+    private boolean canMove;
     private Image image;
     private int xImage2;
     private int yImage2;
     private Image image2;
 
-    public North() {
+    public Mord() {
         x = 0;
         y = 0;
         xImage2=8000;
         yImage2=0;
+        canMove=true;
         
     }
 
@@ -24,6 +31,11 @@ public class North {
       image=li.getImage();  
     }
 
+  public void setCanMoveI(boolean canMove){
+     this. canMove=canMove;  
+    }
+  
+    @Override
   public int getX() {
         return x;
     }
@@ -32,10 +44,12 @@ public class North {
         return xImage2;
     }
 
+    @Override
   public int getY() {
         return y;
     }
 
+    @Override
     public Image getImage() {
         return image;
     } 
@@ -43,12 +57,16 @@ public class North {
       * method to move background
       * x is the x position of background and move method changes the possition by -1 . if position of image is less than 1500 the it reposition at 0.
     */
+    @Override
     public void move() {
-        if (xImage2==0){
-            x=0;
-            xImage2=8000;
-        }            
-        x -=1;
-        xImage2-=1;
+        if (canMove){
+            if (xImage2==0){
+               x=0;
+               xImage2=8000;
+            }            
+           x -=1;
+           xImage2-=1;
+        }
     }
+
 }
