241cb5cedfa24bf4f65c0318283bbaa08981714e
dhirajmalla <replytodhiraj@gmail.com>
2013-05-15 12:32:46 +0300

    Update ShipTest.java

--- test/spacewar/ShipTest.java
+++ test/spacewar/ShipTest.java
@@ -5,52 +5,22 @@
 package spacewar;
 
 import java.awt.Image;
-import java.awt.event.KeyEvent;
-import org.junit.After;
-import org.junit.AfterClass;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  *
- * not fully implemented ; got some errors , we didn't know how to write
- * tests for keyboard input
+ *@author  Dhiraj and Ioana
  */
 public class ShipTest {
     
     public ShipTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    /**
-     * Test of move method, of class Ship.
-     */
-    @Test
-    public void testMove() {
-        System.out.println("move");
-        Ship instance = new Ship();
-        instance.move();
-        // TODO review the generated test code and remove the default call to fail.
-        
-    }
-
+  
     /**
      * Test of getX method, of class Ship.
      */
@@ -58,10 +28,9 @@ public class ShipTest {
     public void testGetX() {
         System.out.println("getX");
         Ship instance = new Ship();
-        int expResult = instance.getX();
+        int expResult = 10;
         int result = instance.getX();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
         
     }
 
@@ -72,10 +41,9 @@ public class ShipTest {
     public void testGetY() {
         System.out.println("getY");
         Ship instance = new Ship();
-        int expResult = instance.getY();
+        int expResult = 400;
         int result = instance.getY();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
        
     }
 
@@ -86,36 +54,123 @@ public class ShipTest {
     public void testGetImage() {
         System.out.println("getImage");
         Ship instance = new Ship();
-        Image expResult =  instance.getImage();
+        ImageIcon li =  new ImageIcon(this.getClass().getResource("\\picture\\craft.png"));
+        instance.setImage(li);
+
+        Image expResult = li.getImage();
         Image result = instance.getImage();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        
     }
 
     /**
-     * Test of keyPressed method, of class Ship.
+     * Test of setImage method, of class Ship.
      */
     @Test
-    public void testKeyPressed() {
-        System.out.println("keyPressed");
-        KeyEvent e = null;
+    public void testSetImage() {
+        System.out.println("setImage");
         Ship instance = new Ship();
-        instance.keyPressed(e);
-        // TODO review the generated test code and remove the default call to fail.
-        
+        ImageIcon li =  new ImageIcon(this.getClass().getResource("\\picture\\craft.png"));
+        instance.setImage(li);
+
+        Image expResult = li.getImage();
+        Image result = instance.getImage();
+        assertEquals(expResult, result);
     }
 
     /**
-     * Test of keyReleased method, of class Ship.
+     * Test of setVisible method, of class Ship.
      */
     @Test
-    public void testKeyReleased() {
-        System.out.println("keyReleased");
-        KeyEvent e = null;
+    public void testSetVisible() {
+        System.out.println("setVisible");
+        boolean visible = false;
         Ship instance = new Ship();
-        instance.keyReleased(e);
-        // TODO review the generated test code and remove the default call to fail.
+        instance.setVisible(visible);
         
+        boolean expResult=false;
+        boolean result= instance.isVisible();
+        assertEquals(expResult, result);        
     }
+
+   
+    /**
+     * Test of setEnergyLevel method, of class Ship.
+     */
+    @Test
+    public void testSetEnergyLevel() {
+        System.out.println("setEnergyLevel");
+        int energyLevel = 40;
+        Ship instance = new Ship();
+        instance.setEnergyLevel(energyLevel);
+   
+        int expResult=40;
+        int result=instance.getEnergyLevel();
+        assertEquals(expResult, result);                
+    }
+
+    /**
+     * Test of getEnergyLevel method, of class Ship.
+     */
+    @Test
+    public void testGetEnergyLevel() {
+        System.out.println("getEnergyLevel");
+        Ship instance = new Ship();
+        int expResult = 100;
+        int result = instance.getEnergyLevel();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getBullets method, of class Ship.
+     */
+    @Test
+    public void testGetBullets() {
+        System.out.println("getBullets");
+        
+        ArrayList bullets = new ArrayList();
+        bullets.add(new Bullet(10,20));
+        int expResult=bullets.size();
+        
+        Ship instance = new Ship();
+        instance.fire();
+        ArrayList bulletReturns= instance.getBullets();
+        int result = bulletReturns.size();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of isVisible method, of class Ship.
+     */
+    @Test
+    public void testIsVisible() {
+        System.out.println("isVisible");
+        Ship instance = new Ship();
+        boolean expResult = true;
+        boolean result = instance.isVisible();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getBounds method, of class Ship.
+     */
+    @Test
+    public void testGetBounds() {
+        System.out.println("getBounds");
+        Ship instance = new Ship();
+       Rectangle expResult = new Rectangle(10, 400, 40,20);
+        Rectangle result = instance.getBounds();
+        assertEquals(expResult, result);
+
+    }
+
+    /**
+     * Test of fire method, of class Ship.
+     */
+    @Test
+    public void testFire() {
+        System.out.println("fire");
+        Ship instance = new Ship();
+        instance.fire();
+    }
+
 }
