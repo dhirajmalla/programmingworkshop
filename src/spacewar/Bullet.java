4cfd3cb4866d300eb8add21e70e82489c74f0850
dhirajmalla <replytodhiraj@gmail.com>
2013-05-15 11:07:06 +0200

    Update Bullet.java

--- src/spacewar/Bullet.java
+++ src/spacewar/Bullet.java
@@ -5,15 +5,26 @@ import java.awt.Image;
 import java.awt.Rectangle;
 import javax.swing.ImageIcon;
 
+/**
+ * This is the class for the bullet
+ * @author Team  Dhiraj  and Ioana
+ * @version 1.0
+ */
 
-public class Bullet {
+public class Bullet implements Player{
     private int speedBullet;
     private int x;
     private int y;
     private boolean visible;
     private Image image;
    
-    //Constructor
+    /**
+     * 
+     * @param x
+     *          specified the x position of the bullet
+     * @param y
+     *          specified the y position of the bullet
+     */         
     public Bullet(int x, int y) {
 
         // initial position of a bullet
@@ -40,32 +51,42 @@ public class Bullet {
         this.visible = visible;
     }    
     
+    @Override
     public int getX() {
         return x;
     }
 
+    @Override
     public int getY() {
         return y;
     }
 
+    @Override
     public boolean isVisible() {
         return visible;
     }
 
+    @Override
     public Image getImage() {
         return image;
     }
 
+    @Override
     public Rectangle getBounds() {
-        //return new Rectangle(x, y, this.image.getWidth(null), this.image.getHeight(null));
         return new Rectangle(x, y, 10,4);
     } 
 
-    // Method to move bullet
+    /**
+     * Method to move bullet
+     * 
+     * */
+     
+    @Override
     public void move () {
             x += speedBullet;
             if (x > 1350){
                 visible = false;
             }
     }
+
 }
