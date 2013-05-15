495cbda0b4e97ff2c573a66a614d1fb4c3868d17
dhirajmalla <replytodhiraj@gmail.com>
2013-05-15 11:32:02 +0200

    Update and rename NorthTest.java to MordTest.java

---
+++ test/spacewar/MordTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacewar;
+
+import java.awt.Image;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Dhiraj and Ioana
+ */
+public class MordTest {
+    
+    public MordTest() {
+    }
+    
+ 
+
+    /**
+     * Test of getX method, of class Mord.
+     */
+    @Test
+    public void testGetX() {
+        System.out.println("getX");
+        Mord instance = new Mord();
+        int expResult =  instance.getX();
+        int result = instance.getX();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getY method, of class Mord.
+     */
+    @Test
+    public void testGetY() {
+        System.out.println("getY");
+        Mord instance = new Mord();
+        int expResult = instance.getY();
+        int result = instance.getY();
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of getImage method, of class Mord.
+     */
+    @Test
+    public void testGetImage() {
+        System.out.println("getImage");
+        Mord instance = new Mord();
+        Image expResult = instance.getImage();
+        Image result = instance.getImage();
+        assertEquals(expResult, result);
+       
+    }
+}
